b3372f70a448bb73423da9a3014632fd6fb3f13b
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2021-08-12 20:56:19 +0200

    created example relations

--- Backend/Backend/AppDbContext.cs
+++ Backend/Backend/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Backend.Models.ShopPanelModels;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +9,46 @@ namespace Backend
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Privilege> Privileges { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<ProfilesPrivileges> ProfilesPrivileges { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //configuration
+            ConfigurePrivileges(modelBuilder);
+            ConfigureProfiles(modelBuilder);
+
+            //relations
+            SetPrivilegesProfilesRelation(modelBuilder);
+        }
+
+        private void ConfigurePrivileges(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Privilege>()
+                .Property(p => p.Name).HasMaxLength(30);
+        }
+        private void ConfigureProfiles(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Profile>()
+                .Property(p => p.Name).HasMaxLength(30);
+        }
+
+        private void SetPrivilegesProfilesRelation(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProfilesPrivileges>()
+                 .HasKey(pp => new { pp.PrivilegeId, pp.ProfileId });
+            modelBuilder.Entity<ProfilesPrivileges>()
+                .HasOne(pp => pp.Profile)
+                .WithMany(p => p.ProfilesPrivileges)
+                .HasForeignKey(pp => pp.ProfileId)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<ProfilesPrivileges>()
+                .HasOne(pp => pp.Privilege)
+                .WithMany(p => p.ProfilesPrivileges)
+                .HasForeignKey(pp => pp.PrivilegeId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
