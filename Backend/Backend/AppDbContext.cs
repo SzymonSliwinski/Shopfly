3e3a2ab5f7a8173825b9df2f1016c646551a7e69
michal1298 <michal.jank98@gmail.com>
2021-08-15 16:57:10 +0200

    Add shop panel models configuration and relations

--- Backend/Backend/AppDbContext.cs
+++ Backend/Backend/AppDbContext.cs
@@ -19,24 +19,41 @@ namespace Backend
             //configuration
             ConfigurePrivileges(modelBuilder);
             ConfigureProfiles(modelBuilder);
+            ConfigureEmployees(modelBuilder);
 
             //relations
             SetPrivilegesProfilesRelation(modelBuilder);
+            SetEmplooyesProfilesRelation(modelBuilder);
         }
 
+        // konfiguracje:
         private void ConfigurePrivileges(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Privilege>()
                 .Property(p => p.Name).HasMaxLength(30);
         }
+
         private void ConfigureProfiles(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Profile>()
                 .Property(p => p.Name).HasMaxLength(30);
         }
 
+        private void ConfigureEmployees(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Name).HasMaxLength(50);
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Surname).HasMaxLength(50);
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Email).HasMaxLength(30);
+            // todo tutaj prawdopodobnie trzeba będzie zrobić właściwość do hasła
+        }
+
+        // relacje:
         private void SetPrivilegesProfilesRelation(ModelBuilder modelBuilder)
         {
+            // relacja many to many:
             modelBuilder.Entity<ProfilesPrivileges>()
                  .HasKey(pp => new { pp.PrivilegeId, pp.ProfileId });
             modelBuilder.Entity<ProfilesPrivileges>()
@@ -48,6 +65,17 @@ namespace Backend
                 .HasOne(pp => pp.Privilege)
                 .WithMany(p => p.ProfilesPrivileges)
                 .HasForeignKey(pp => pp.PrivilegeId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        private void SetEmplooyesProfilesRelation(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeesProfiles>()
+                .HasKey(ep => new { ep.EmployeeId, ep.ProfileId });
+            modelBuilder.Entity<EmployeesProfiles>()
+                .HasOne(ep => ep.Profile)
+                .WithMany(ep => ep.EmployeesProfiles)
+                .HasForeignKey(ep => ep.ProfileId)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
--- Backend/Backend/Models/ShopPanelModels/Profile.cs
+++ Backend/Backend/Models/ShopPanelModels/Profile.cs
@@ -5,7 +5,7 @@ namespace Backend.Models.ShopPanelModels
     public class Profile
     {
         public int Id { get; set; }
-        public string Name { get; set; }    // todo length of string
+        public string Name { get; set; }
         public List<ProfilesPrivileges> ProfilesPrivileges { get; set; }
         public List<EmployeesProfiles> EmployeesProfiles { get; set; }
     }
