e34f444c59ce8a5bb6ebdef72309c16d29b9ba10
michal1298 <michal.jank98@gmail.com>
2021-10-04 21:39:53 +0200

    Add new project - generate random data

---
+++ Backend/GenerateRandomData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenerateRandomData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
