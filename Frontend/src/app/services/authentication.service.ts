3ad32d1cee1d4eb37e60ea13843f0f2ac81e1da3
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2021-10-12 19:11:42 +0200

    added basic styles classes, order mockup

--- Frontend/src/app/services/authentication.service.ts
+++ Frontend/src/app/services/authentication.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from "@angular/core";
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from "@angular/router";
 import { environment } from "src/environments/environment";
-
+import { PanelAuthenticationService } from "./shop-panel-services/panel-authentication.service";
 @Injectable()
 export class AuthenticationService implements CanActivate {
     constructor(
         private router: Router,
+        private readonly panelAuthService: PanelAuthenticationService
     ) { }
     canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot)
         : boolean | Promise<boolean> {
         const storage = JSON.parse(localStorage.getItem(environment._panelStorageKey)!) as { token: { expirationDate: Date; userId: number; value: string } };
         if (storage && new Date(storage.token.expirationDate).getTime() > new Date().getTime())
             return true;
-        if (storage && new Date(storage.token.expirationDate).getTime() < new Date().getTime())
+        if (storage && new Date(storage.token.expirationDate).getTime() < new Date().getTime()) {
+            this.panelAuthService.logout();
             localStorage.removeItem(environment._shopStorageKey);
+        }
 
         return this.router.navigate(['panel/sign-in']);
     }
