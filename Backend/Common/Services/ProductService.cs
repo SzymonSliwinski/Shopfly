72b0ead4ced8cd7a804c25009df4c88a39ef01c2
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2021-10-24 12:38:36 +0200

    Created example services and controllers

--- Backend/Common/Services/ProductService.cs
+++ Backend/Common/Services/ProductService.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Common.Models.ShopModels;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Common.Services
@@ -11,6 +12,60 @@ namespace Common.Services
         public ProductService(AppDbContext context)
         {
             _context = context;
+        }
+
+        public async Task<Product> GetById(int id)
+        {
+            var result = await _context.Products
+                .AsQueryable()
+                .SingleAsync(p => p.Id == id);
+
+            return result;
+        }
+
+        public async Task<Product> Delete(int id)
+        {
+            var productDb = await GetById(id);
+            productDb.IsActive = false;
+            await _context.SaveChangesAsync();
+
+            return productDb;
+        }
+
+        public async Task<Product> Add(Product product)
+        {
+            product.Name = product.Name.Trim();
+            product.IsActive = true;
+            product.CreateDate = DateTime.Now.ToLocalTime();
+            product.UpdateDate = DateTime.Now.ToLocalTime();
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<List<Product>> GetAll()
+        {
+            return await _context.Products
+                .AsQueryable()
+                .ToListAsync();
+        }
+
+        public async Task<Product> Update(Product updatedProduct)
+        {
+            var oldProduct = await GetById(updatedProduct.Id);
+            oldProduct.CategoryId = updatedProduct.CategoryId;
+            oldProduct.Name = updatedProduct.Name.Trim();
+            oldProduct.TaxId = updatedProduct.TaxId;
+            oldProduct.IsLowStock = updatedProduct.IsLowStock;
+            oldProduct.AdditionalShippingCost = updatedProduct.AdditionalShippingCost;
+            oldProduct.NettoPrice = updatedProduct.NettoPrice;
+            oldProduct.BruttoPrice = updatedProduct.BruttoPrice;
+            oldProduct.IsVisible = updatedProduct.IsVisible;
+            oldProduct.Description = updatedProduct.Description;
+            oldProduct.UpdateDate = DateTime.Now.ToLocalTime();
+
+            await _context.SaveChangesAsync();
+            return oldProduct;
         }
     }
 }
--- Backend/ShopPanelWebApi/Controllers/ProductController.cs
+++ Backend/ShopPanelWebApi/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using Common.Models.ShopModels;
 using Common.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +20,36 @@ namespace ShopPanelWebApi.Controllers
         public ProductController(AppDbContext context)
         {
             _productService = new ProductService(context);
+        }
+
+        [HttpGet("by-id/{id}")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            return Ok(await _productService.GetById(id));
+        }
+
+        [HttpGet("get-all")]
+        public async Task<ActionResult<Product>> GetAll()
+        {
+            return Ok(await _productService.GetAll());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> Delete(int id)
+        {
+            return Ok(await _productService.Delete(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Add([FromBody] Product product)
+        {
+            return Ok(await _productService.Add(product));
+        }
+
+        [HttpPatch]
+        public async Task<ActionResult<Product>> Update([FromBody] Product product)
+        {
+            return Ok(await _productService.Update(product));
         }
     }
 }
