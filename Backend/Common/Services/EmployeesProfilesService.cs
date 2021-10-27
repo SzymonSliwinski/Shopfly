f420fe80547a0d1c1943b94a01269d70f6eb9f6f
michal1298 <michal.jank98@gmail.com>
2021-10-27 21:27:42 +0200

    Add services and controlers from Shop Panel Models

---
+++ Backend/Common/Services/EmployeesProfilesService.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Common.Models.ShopModels;
+using Common.Models.ShopPanelModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Common.Services
+{
+    public class EmployeesProfilesService
+    {
+        private readonly AppDbContext _context;
+        public EmployeesProfilesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<EmployeesProfiles> FindOne(int employeeId, int profileId)
+        {
+            var result = await _context.EmployeesProfiles
+                .AsQueryable()
+                .SingleAsync(ep => ep.EmployeeId == employeeId && ep.ProfileId == profileId);
+
+            return result;
+        }
+
+        public async Task<EmployeesProfiles> Delete(int employeeId, int profileId)
+        {
+            var employeesProfilesDb = await FindOne(employeeId, profileId);
+            _context.Remove(employeesProfilesDb);
+            await _context.SaveChangesAsync();
+
+            return employeesProfilesDb;
+        }
+
+        public async Task<EmployeesProfiles> Add(EmployeesProfiles employeesProfiles)
+        {
+            var newEmployeesProfiles = new EmployeesProfiles()
+            {
+                EmployeeId = employeesProfiles.EmployeeId,
+                ProfileId = employeesProfiles.ProfileId
+            };
+
+            await _context.AddAsync(newEmployeesProfiles);
+            await _context.SaveChangesAsync();
+
+            return newEmployeesProfiles;
+        }
+
+        public async Task<EmployeesProfiles> Update(EmployeesProfiles oldEmployeesProfiles, EmployeesProfiles newEmployeesProfiles)
+        {
+            await Delete(oldEmployeesProfiles.EmployeeId, oldEmployeesProfiles.ProfileId);
+            await Add(newEmployeesProfiles);
+            return newEmployeesProfiles;
+        }
+
+        public async Task<List<EmployeesProfiles>> AddMany(List<EmployeesProfiles> employeesProfilesList)
+        {
+            foreach (var employeesProfiles in employeesProfilesList)
+                await Add(employeesProfiles);
+
+            return employeesProfilesList;
+        }
+
+        public async Task<List<EmployeesProfiles>> RemoveMany(List<EmployeesProfiles> employeesProfilesList)
+        {
+            foreach (var employeesProfiles in employeesProfilesList)
+                await Delete(employeesProfiles.EmployeeId, employeesProfiles.ProfileId);
+
+            return employeesProfilesList;
+        }
+    }
+}
---
+++ Backend/ShopPanelWebApi/Controllers/PrivilegeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Common;
+using Common.Models.ShopPanelModels;
+using Common.Services;
+using ShopPanelWebApi.Filters;
+
+namespace ShopPanelWebApi.Controllers
+{
+    [Route("shop-panel/[controller]")]
+    [TokenAuthenticationFilter]
+    [ApiController]
+    public class PrivilegeController : ControllerBase
+    {
+        private readonly PrivilegeService _privilegeService;
+        public PrivilegeController(AppDbContext context)
+        {
+            _privilegeService = new PrivilegeService(context);
+        }
+
+        [HttpGet("by-id/{id}")]
+        public async Task<ActionResult<Privilege>> GetById(int id)
+        {
+            return Ok(await _privilegeService.GetById(id));
+        }
+
+        [HttpGet("get-all")]
+        public async Task<ActionResult<Privilege>> GetAll()
+        {
+            return Ok(await _privilegeService.GetAll());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Privilege>> Delete(int id)
+        {
+            return Ok(await _privilegeService.Delete(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Privilege>> Add([FromBody] Privilege privilege)
+        {
+            return Ok(await _privilegeService.Add(privilege));
+        }
+
+        [HttpPatch]
+        public async Task<ActionResult<Privilege>> Update([FromBody] Privilege privilege)
+        {
+            return Ok(await _privilegeService.Update(privilege));
+        }
+    }
+}
