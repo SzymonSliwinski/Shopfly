cbb5dcd13e8b4b36f5395ab06d06e3fe9f209bbe
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2021-11-04 20:28:15 +0100

    import mockup

--- Frontend/src/app/components/shop-panel/import/import.module.ts
+++ Frontend/src/app/components/shop-panel/import/import.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { ImportComponent } from './import.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [
@@ -12,6 +17,11 @@ import { ImportComponent } from './import.component';
     imports: [
         CommonModule,
         MatIconModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        FormsModule,
+        MatInputModule,
+        MatButtonModule
     ],
     providers: [
     ],
