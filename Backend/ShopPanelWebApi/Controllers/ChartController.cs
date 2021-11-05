85dcb037e4917020d0a825d6b72380389c403143
michal1298 <michal.jank98@gmail.com>
2021-11-05 20:49:28 +0100

    Add order chart data

---
+++ Backend/ShopPanelWebApi/Controllers/ChartController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Common;
+using Common.Dtos;
+using ShopPanelWebApi.Filters;
+using ShopPanelWebApi.Services;
+
+namespace ShopPanelWebApi.Controllers
+{
+    [Route("shop-panel/[controller]")]
+    [TokenAuthenticationFilter]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private readonly ChartsService _chartsService;
+        public ChartController(AppDbContext context)
+        {
+            _chartsService = new ChartsService(context);
+        }
+        
+        [HttpPost("order-chart-data")]
+        public async Task<ActionResult<Dictionary<DateTime, int>>> GetOrderChartData([FromBody] DataRangeDto dataRange)
+        {
+            return Ok(await _chartsService.GetOrdersChartDataFromDays(dataRange.From, dataRange.To));
+        }
+
+    }
+}
---
+++ Backend/ShopPanelWebApi/Services/ChartsService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopPanelWebApi.Services
+{
+    public class ChartsService
+    {
+        private readonly AppDbContext _context;
+        public ChartsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Dictionary<DateTime, int>> GetOrdersChartDataFromDays(DateTime dateFrom, DateTime dateTo)
+        {
+            var ordersFromDays = await _context.Orders
+                .AsQueryable()
+                .Where(c => c.Date >= dateFrom.ToLocalTime() && c.Date <= dateTo.ToLocalTime())
+                .ToListAsync();
+            var result = new Dictionary<DateTime, int>();
+
+            for(var day = dateFrom.Date; day.Date <= dateTo.Date; day = day.AddDays(1)) // wypełnia cały słownik zakresem dat
+                result.Add(day.Date, 0);
+
+            foreach(var orderFromDays in ordersFromDays)
+                result[orderFromDays.Date]++;
+
+            return result;
+        }
+    }
+}
