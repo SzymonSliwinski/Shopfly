0e83921295837c8bbbc6abccb07e8bf4c61f2d6a
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2021-11-07 17:38:04 +0100

    initialized profiles

--- Frontend/src/app/components/shop-panel/employees/profiles-list/profiles-list.component.ts
+++ Frontend/src/app/components/shop-panel/employees/profiles-list/profiles-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuButton, TableButton } from 'src/app/components/shared/data-table/data-table.component';
+import { ContentMode, TableColumnDto } from 'src/app/dto/table-column.dto';
+import { Profile } from 'src/app/models/shop-panel-models/profile.model';
 
 @Component({
   selector: 'app-profiles-list',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profiles-list.component.scss']
 })
 export class ProfilesListComponent implements OnInit {
+  public profilesList!: Profile[];
 
+  public isChoosenElementVisible!: boolean;
+  isLoaded = false;
   constructor() { }
+  public tableButtons: TableButton[] = [TableButton.Edit, TableButton.Menu];
+  public menuButtons: MenuButton[] = [MenuButton.Delete, MenuButton.Details];
+  public displayedColumns: TableColumnDto[] =
+    [
+      { title: 'ID', objectField: 'id' },
+      { title: 'Name', objectField: 'name' },
+      { title: '', objectField: 'buttons', contentMode: ContentMode.Buttons },
+    ];
+  public columnsNames: string[] = [];
 
   ngOnInit(): void {
-  }
+    this.isLoaded = false;
+    this.profilesList = [{
+      id: 1,
+      name: 'Admini',
+    },
+    {
+      id: 2,
+      name: 'Obsługa zamówień',
 
+    }];
+    this.displayedColumns.forEach(c => {
+      this.columnsNames.push(c.objectField!);
+    });
+    this.isLoaded = true;
+  }
 }
