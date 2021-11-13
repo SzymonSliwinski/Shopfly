a58b7a027a0d203c0b69d473e1540f59f9ea1149
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2021-11-13 18:32:46 +0100

    sign up form mockup

--- Frontend/src/app/components/shop/sign-in/sign-in.module.ts
+++ Frontend/src/app/components/shop/sign-in/sign-in.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
     declarations: [
         SignInComponent,
@@ -10,6 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     imports: [
         CommonModule,
         FormsModule,
+        MatButtonModule,
         ReactiveFormsModule
     ],
     providers: [
--- Frontend/src/app/components/shop/sign-up/sign-up.module.ts
+++ Frontend/src/app/components/shop/sign-up/sign-up.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
     declarations: [
         SignUpComponent,
     ],
     imports: [
         CommonModule,
+        MatButtonModule,
+        MatCheckboxModule
     ],
     providers: [
     ],
