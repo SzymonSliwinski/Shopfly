8e62ce254bbfd8333ff69f846bbdfe909262561e
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2021-11-13 14:49:47 +0100

    shop sign in form

--- Frontend/src/app/components/shop/sign-in/sign-in.component.ts
+++ Frontend/src/app/components/shop/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationDto } from 'src/app/dto/authentication.dto';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  public form: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+  wrongCredentialsError: boolean = false
+  get email() { return this.form.get('email'); }
+  get password() { return this.form.get('password'); }
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.redirectIfSignedIn();
   }
 
+  redirectIfSignedIn(): void {
+
+  }
+
+  async onSubmit() {
+
+
+  }
 }
--- Frontend/src/app/components/shop/sign-in/sign-in.module.ts
+++ Frontend/src/app/components/shop/sign-in/sign-in.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
     declarations: [
         SignInComponent,
     ],
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [
     ],
