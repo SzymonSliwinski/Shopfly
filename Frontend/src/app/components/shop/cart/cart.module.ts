d9670eb117e1bf75c62e1139eb1df4833e9b9c25
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2021-11-22 19:18:03 +0100

    fixed style, layout of cart

--- Frontend/src/app/components/shop/cart/cart.module.ts
+++ Frontend/src/app/components/shop/cart/cart.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 @NgModule({
     declarations: [
         CartComponent,
@@ -9,6 +11,8 @@ import { MatButtonModule } from '@angular/material/button';
     imports: [
         CommonModule,
         MatButtonModule,
+        MatIconModule,
+        MatDividerModule
     ],
     providers: [
     ],
