7c2eddaa56cb4e575b873995c77fe6e0392b480f
michal1298 <michal.jank98@gmail.com>
2022-01-09 16:35:30 +0100

    Add check extension uploaded file

---
+++ Backend/ShopPanelWebApi/Controllers/ImportController.cs
@@ -0,0 +1,46 @@
+﻿using Common.Models.ApiModels;
+using Common.Utilieties;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopPanelWebApi.Controllers
+{
+    [Route("shop-panel/[controller]")]
+    [ApiController]
+    public class ImportController : ControllerBase
+    {
+        [HttpPost("table-type/{tableTypes}")]
+        public async Task<ActionResult> GetExtension(IFormFile file, TableType tableTypes)
+        {
+            if (file.Length > 0)
+            {
+                var fileExtension = file.FileName.Split(".").Last();
+
+                if (fileExtension == "json")
+                {
+                    // wywołanie parsowania?
+                    // sprawdzenie, do jakiej tabeli ma to iść. na podstawie enuma.
+                    // jeśli ktoś wrzuci zły plik, trzeba zrobić walidację pliku, czy ma dobre kolumny.
+
+                    ShopPanelWebApi.Repositories.FileRepository.CheckFile(file, tableTypes, fileExtension);
+                }
+                else if (fileExtension == "csv")
+                {
+                    ShopPanelWebApi.Repositories.FileRepository.CheckFile(file, tableTypes, fileExtension);
+                }
+                else
+                    return new UnsupportedMediaTypeResult();    // zwraca 415
+            }
+            else
+                return new UnsupportedMediaTypeResult();
+
+            return new UnsupportedMediaTypeResult();
+        }
+    }
+
+}
---
+++ Backend/ShopPanelWebApi/Repositories/FileRepository.cs
@@ -0,0 +1,149 @@
+﻿using Common.Models.ApiModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Models.ShopPanelModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopPanelWebApi.Repositories
+{
+    public class FileRepository
+    {
+        public static void CheckFile(IFormFile file, TableType tableType, string extension)
+        {
+            switch (tableType)
+            {
+                case TableType.employees:
+                    if (extension == ".json")
+                    {
+                        var parseContent = Newtonsoft.Json.JsonConvert.DeserializeObject<Employee>(file.ToString());
+                    }
+
+                    if (extension == ".csv")
+                    {
+
+                    }
+                    else
+                        new UnsupportedMediaTypeResult();
+                    break;
+
+                case TableType.employeesProfiles:
+
+                    break;
+
+                case TableType.profiles:
+
+                    break;
+
+                case TableType.profilesPrivileges:
+
+                    break;
+
+                case TableType.privileges:
+
+                    break;
+
+                case TableType.customers:
+
+                    break;
+
+                case TableType.statuses:
+
+                    break;
+
+                case TableType.orders:
+
+                    break;
+
+                case TableType.ordersProducts:
+
+                    break;
+
+                case TableType.customrFavouritesProducts:
+
+                    break;
+
+                case TableType.ratings:
+
+                    break;
+
+                case TableType.comments:
+
+                    break;
+
+                case TableType.tags:
+
+                    break;
+
+                case TableType.productTags:
+
+                    break;
+
+                case TableType.products:
+
+                    break;
+
+                case TableType.productsPayments:
+
+                    break;
+
+                case TableType.paymentTypes:
+
+                    break;
+
+                case TableType.carriers:
+
+                    break;
+
+                case TableType.productsCarriers:
+
+                    break;
+
+                case TableType.taxes:
+
+                    break;
+
+                case TableType.categories:
+
+                    break;
+
+                case TableType.productsVariants:
+
+                    break;
+
+                case TableType.productsVariantsPhotos:
+
+                    break;
+
+                case TableType.photos:
+
+                    break;
+
+                case TableType.productColors:
+
+                    break;
+
+                case TableType.productDiemensions:
+
+                    break;
+
+
+
+
+                default:
+                    new UnsupportedMediaTypeResult();
+                    break;
+                        
+            }
+        }
+
+        //public static CheckTable()
+        //{
+
+        //}
+        
+    }
+}
