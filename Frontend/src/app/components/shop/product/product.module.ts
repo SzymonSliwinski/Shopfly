bcbaa5008c6981601d73f3715d0e76a298241479
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2022-01-28 20:52:26 +0100

    products mockup idea

---
+++ Frontend/src/app/components/shop/product/product.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { ProductsService } from 'src/app/services/shop/product.service';
+import { PaginatorModule } from 'primeng/paginator';
+import { ProductComponent } from './product.component';
+@NgModule({
+    declarations: [
+        ProductComponent
+    ],
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatIconModule,
+        MatDividerModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRadioModule,
+        MatCheckboxModule,
+        MatSelectModule,
+        PaginatorModule
+    ],
+    providers: [
+        ProductsService
+    ],
+    exports: [
+        ProductComponent
+    ]
+})
+
+export class ProductModule { }
