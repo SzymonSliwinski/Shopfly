318dc4a588de725c8007a729b3b5472807300b0e
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2022-02-02 20:07:21 +0100

    finished dynamic fv content

--- Backend/Common/Services/FvService.cs
+++ Backend/Common/Services/FvService.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Common.Models.ShopModels;
+using FSharp.Data.Runtime.BaseTypes;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using OpenHtmlToPdf;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Common.Services
@@ -28,6 +30,7 @@ namespace Common.Services
                 .Include(c => c.OrdersProducts)
                 .ThenInclude(c => c.Product)
                 .Include(c => c.Customer)
+                .Include(c => c.Carrier)
                 .SingleAsync();
             DateTimeOffset d = order.Date;
             html = html.Replace("$$fvNumber$$", d.ToUnixTimeSeconds().ToString());
@@ -36,15 +39,14 @@ namespace Common.Services
             html = html.Replace("$$nip$$", order.Nip != null ? "<span class=\"bolded-font\">NIP:</span>" + order.Nip + "<br>" : "");
             html = html.Replace("$$customerPhone$$", order.CustomerPhoneNumber);
             html = html.Replace("$$customerEmail$$", order.CustomerEmail);
-            html = html.Replace("$$totalVat$$", (order.PriceTotal * 0.23).ToString());
-            html = html.Replace("$$totalNetto$$", (order.PriceTotal * 0.77).ToString());
-            html = html.Replace("$$totalBrutto$$", order.PriceTotal.ToString());
+            html = html.Replace("$$totalVat$$", (order.PriceTotal * 0.23).ToString() + "$");
+            html = html.Replace("$$totalNetto$$", (order.PriceTotal * 0.77).ToString() + "$");
+            html = html.Replace("$$totalBrutto$$", order.PriceTotal.ToString() + "$");
             html = html.Replace("$$customerStreet$$", order.BillingAddressStreet == null ? order.DeliveryAddressStreet : order.BillingAddressStreet);
             html = html.Replace("$$customerPostal$$", order.BillingAddressPostal == null ? order.DeliveryAddressPostal : order.DeliveryAddressPostal);
             html = html.Replace("$$customerCity$$", order.BillingAddressCity == null ? order.DeliveryAddressCity : order.BillingAddressCity);
             html = html.Replace("$$customerCountry$$", order.BillingAddressCountry == null ? order.DeliveryAddressCountry : order.BillingAddressCountry);
-
-
+            html = html.Replace("$$content$$", GetHtmlTable(order));
             return Pdf
                 .From(html)
                 .OfSize(PaperSize.A4)
@@ -52,6 +54,40 @@ namespace Common.Services
                 .WithoutOutline()
                 .Comressed()
                 .Content();
+        }
+
+        private string GetHtmlTable(Order order)
+        {
+            var result = new StringBuilder();
+            result.Append("<table class=\"order-table\">");
+            //headers
+            result.Append("<tr class=\"table-main\">");
+            result.Append("<td>No</td>");
+            result.Append("<td>ID</td>");
+            result.Append("<td>Product name</td>");
+            result.Append("<td>Quantity</td>");
+            result.Append("<td>VAT</td>");
+            result.Append("<td>Netto</td>");
+            result.Append("<td>Brutto</td>");
+            result.Append("</tr>");
+            var iter = 1;
+            foreach (var op in order.OrdersProducts)
+            {
+                result.Append("<tr class=\"table-product\">");
+                result.Append("<td>" + iter.ToString() + "</td>");
+                result.Append("<td>" + op.ProductId + "</td>");
+                result.Append("<td>" + op.Product.Name + "</td>");
+                result.Append("<td>" + op.ProductQuantity + "</td>");
+                result.Append("<td>" + Math.Round((op.Product.BruttoPrice * 0.23), 2).ToString() + "</td>");
+                result.Append("<td>" + Math.Round((op.Product.BruttoPrice * 0.77), 2).ToString() + "</td>");
+                result.Append("<td>" + Math.Round((op.Product.BruttoPrice), 2).ToString() + "</td>");
+                result.Append("</tr>");
+                iter++;
+            }
+
+            result.Append("</table>");
+
+            return result.ToString();
         }
     }
 }
